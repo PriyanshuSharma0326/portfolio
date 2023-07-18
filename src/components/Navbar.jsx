59ada221fbcf788dcdb6e3a35e56d8264b3cba85
PriyanshuSharma0326 <ps940016@gmail.com>
2023-07-18 23:47:13 +0530

    Add Navigation menu for max-width:426px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ import { Link } from 'react-scroll';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import NavigationMenu from './NavigationMenu';
 
 function Navbar() {
     const [show, setShow] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     const transitionNavbar = () => {
         if(window.scrollY > 50) {
@@ -15,6 +17,19 @@ function Navbar() {
         } else {
             setShow(false);
         }
+    }
+
+    const displayMenu = () => {
+        if(menu) {
+            setMenu(false);
+        }
+        else {
+            setMenu(true);
+        }
+    }
+
+    const hideMenu = () => {
+        setMenu(false);
     }
 
     useEffect(() => {
@@ -26,52 +41,62 @@ function Navbar() {
     }, []);
 
     return (
-        <div className={`navbar ${show && 'navbar-black'}`}>
-            <div className="navbar-contents">
-                <div className="nav-logo">
-                    <a href='https://www.github.com/priyanshusharma0326' className="nav-title" target='_blank' rel='noreferrer'>Priyanshu sharma</a>
-                </div>
+        <>
+            <div className={`navbar ${show && 'navbar-black'}`}>
+                <div className="navbar-contents">
+                    <div className="nav-logo">
+                        <a href='https://www.github.com/priyanshusharma0326' className="nav-title" target='_blank' rel='noreferrer'>Priyanshu sharma</a>
+                    </div>
 
-                <div className="nav-links">
-                    <Link 
-                        className="nav-link" 
-                        to="About" 
-                        smooth={true} 
-                        duration={100} 
-                    >
-                        About
-                    </Link>
-                    <Link 
-                        className="nav-link" 
-                        to="Skills" 
-                        smooth={true} 
-                        duration={100} 
-                    >
-                        Skills
-                    </Link>
-                    <Link 
-                        className="nav-link" 
-                        to="Projects" 
-                        smooth={true} 
-                        duration={100} 
-                    >
-                        Projects
-                    </Link>
-                    <Link 
-                        className="nav-link" 
-                        to="Contact" 
-                        smooth={true} 
-                        duration={100} 
-                    >
-                        Contact
-                    </Link>
-                </div>
+                    <div className="nav-links">
+                        <Link 
+                            className="nav-link" 
+                            to="About" 
+                            smooth={true} 
+                            duration={100} 
+                        >
+                            About
+                        </Link>
+                        <Link 
+                            className="nav-link" 
+                            to="Skills" 
+                            smooth={true} 
+                            duration={100} 
+                        >
+                            Skills
+                        </Link>
+                        <Link 
+                            className="nav-link" 
+                            to="Projects" 
+                            smooth={true} 
+                            duration={100} 
+                        >
+                            Projects
+                        </Link>
+                        <Link 
+                            className="nav-link" 
+                            to="Contact" 
+                            smooth={true} 
+                            duration={100} 
+                        >
+                            Contact
+                        </Link>
+                    </div>
 
-                <div className="section-menu">
-                    <FontAwesomeIcon className='fa-bars' icon={faBars} />
+                    <div className="section-menu">
+                        <FontAwesomeIcon 
+                            className='fa-bars' 
+                            icon={faBars} 
+                            onClick={displayMenu}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {menu && <NavigationMenu 
+                hideMenu={hideMenu} 
+            />}
+        </>
     );
 }
 
---
+++ src/components/NavigationMenu.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../styles/NavigationMenu.css';
+
+import { Link } from 'react-scroll';
+
+function NavigationMenu({ hideMenu }) {
+    return (
+        <div className="nav-menu">
+            <div className="nav-menu-links">
+                <Link 
+                    className="nav-menu-link" 
+                    to="About" 
+                    smooth={true} 
+                    duration={100} 
+                    onClick={hideMenu} 
+                >
+                    About
+                </Link>
+                <Link 
+                    className="nav-menu-link" 
+                    to="Skills" 
+                    smooth={true} 
+                    duration={100} 
+                    onClick={hideMenu} 
+                >
+                    Skills
+                </Link>
+                <Link 
+                    className="nav-menu-link" 
+                    to="Projects" 
+                    smooth={true} 
+                    duration={100} 
+                    onClick={hideMenu} 
+                >
+                    Projects
+                </Link>
+                <Link 
+                    className="nav-menu-link" 
+                    to="Contact" 
+                    smooth={true} 
+                    duration={100} 
+                    onClick={hideMenu} 
+                >
+                    Contact
+                </Link>
+                </div>
+        </div>
+    );
+}
+
+export default NavigationMenu;

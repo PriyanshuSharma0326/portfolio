96f03c9a99b4d7d3c825833a7244651b6d665d87
PriyanshuSharma0326 <ps940016@gmail.com>
2023-10-23 14:56:43 +0530

    Update css

--- src/components/about-section/about-section.component.jsx
+++ src/components/about-section/about-section.component.jsx
@@ -10,7 +10,7 @@ function AboutSection() {
             />
 
             <p className="about-description">
-            Hello there! I'm a 21-year-old guy with a love for web development. I enjoy creating beautiful and accurate websites that you can see and interact with. Learning about new technologies excites me, and I always keep an eye out for the latest trends. When I'm not working on websites, I like to explore my artistic side by drawing incredibly realistic pencil sketches.
+            Hello there! I'm a 21-year-old guy with a love for web development. I enjoy creating beautiful and accurate websites that you can see and interact with. Learning about new technologies excites me, and I always keep an eye out for the latest trends.
             <br />
             <br />
             Welcome to my portfolio website, where I share my passion for both web development and art!

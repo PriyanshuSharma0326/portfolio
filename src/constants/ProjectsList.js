305f5947a77425b66678e2ab0a00711a934dda32
Priyanshu Sharma <54506478+PriyanshuSharma0326@users.noreply.github.com>
2023-11-18 14:51:13 +0530

    Update ProjectsList.js

--- src/constants/ProjectsList.js
+++ src/constants/ProjectsList.js
@@ -1,74 +1,91 @@
 const projects = [
     {
         id: 1,
+        project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/electronics-store-react/main/public/assets/desktop-siteshot.png',
+        project_title: 'CircuitCart',
+        live_url: 'https://circuit-cart.vercel.app/',
+        source_url: 'https://github.com/PriyanshuSharma0326/electronics-store-react',
+        description: 'CircuitCart is your virtual electronics store, designed to make your shopping experience effortless and enjoyable. Explore various categories, find your favorite products, and add them to your cart with ease.',
+        stack: [
+            'React',
+            'ContextAPI',
+            'SASS',
+            'Firebase',
+            'React Router'
+        ]
+    },
+    {
+        id: 2,
         project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/telechat/main/public/media/desktop-siteshot.png',
         project_title: 'Telechat',
         live_url: 'https://telechat-connect.vercel.app',
         source_url: 'https://github.com/PriyanshuSharma0326/telechat',
-    },
-    {
-        id: 2,
-        project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/electronics-store-react/main/public/assets/desktop-siteshot.png',
-        project_title: 'Circuit Cart',
-        live_url: 'https://circuit-cart.vercel.app/',
-        source_url: 'https://github.com/PriyanshuSharma0326/electronics-store-react',
+        description: 'A real-time web-based chat application offering users an interactive communication platform that\'s fully responsive on all devices.',
+        stack: [
+            'React',
+            'ContextAPI',
+            'SASS',
+            'MaterialUI',
+            'Firebase',
+            'React Router'
+        ]
     },
     {
         id: 3,
+        project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/react-news-app/main/public/assets/desktop-siteshot.png',
+        project_title: 'ReactReporter',
+        live_url: 'https://react-reporter-app.vercel.app/',
+        source_url: 'https://github.com/PriyanshuSharma0326/react-news-app',
+        description: 'A responsive News Aggregator application with curated news articles fetched from the API.',
+        stack: [
+            'React',
+            'Redux',
+            'SASS',
+            'NewsAPI',
+            'Firebase',
+            'React Router',
+            'Axios'
+        ]
+    },
+    {
+        id: 4,
         project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/tic-tac-toe/main/design/desktop-design.png',
         project_title: 'Tic Tac Toe',
         live_url: 'https://priyanshusharma0326.github.io/tic-tac-toe/',
         source_url: 'https://github.com/PriyanshuSharma0326/tic-tac-toe',
+        description: 'The popular Tic Tac Toe game built on JavaScript. The game allows users to compete against the computer on a 3x3 grid.',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
     },
     {
-        id: 4,
+        id: 5,
         project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/pomodoro-clock/main/design/desktop-design.png',
         project_title: 'Pomodoro Clock',
         live_url: 'https://priyanshusharma0326.github.io/pomodoro-clock/',
         source_url: 'https://github.com/PriyanshuSharma0326/pomodoro-clock',
+        description: 'Pomodoro application helps users manage their time using the Pomodoro Technique, a time management method developed by Francesco Cirillo.',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
     },
-    // {
-    //     id: 5,
-    //     project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/Random-Advice-Generator/main/design/desktop-design.png',
-    //     project_title: 'Random Advice Generator',
-    //     live_url: 'https://priyanshusharma0326.github.io/Random-Advice-Generator/',
-    //     source_url: 'https://github.com/PriyanshuSharma0326/Random-Advice-Generator',
-    // },
     {
         id: 6,
         project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/expense-tracker/main/design/desktop-design.png',
         project_title: 'Expense Tracker',
         live_url: 'https://priyanshusharma0326.github.io/expense-tracker/',
         source_url: 'https://github.com/PriyanshuSharma0326/expense-tracker',
+        description: 'This expense tracker application allows users to track their spendings.',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
     },
-    // {
-    //     id: 7,
-    //     project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/tip-calculator/main/design/desktop-design.png',
-    //     project_title: 'Tip Calculator',
-    //     live_url: 'https://priyanshusharma0326.github.io/tip-calculator/',
-    //     source_url: 'https://github.com/PriyanshuSharma0326/tip-calculator',
-    // },
-    // {
-    //     id: 8,
-    //     project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/vanilla-weather/main/design/desktop-design.jpeg',
-    //     project_title: 'Vanilla Weather',
-    //     live_url: 'https://vanilla-weather-xtechilad.netlify.app/',
-    //     source_url: 'https://github.com/PriyanshuSharma0326/vanilla-weather',
-    // },
-    // {
-    //     id: 9,
-    //     project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/Guess-My-Number/main/design/desktop-design.png',
-    //     project_title: 'Guess the Number',
-    //     live_url: 'https://priyanshusharma0326.github.io/Guess-My-Number/',
-    //     source_url: 'https://github.com/PriyanshuSharma0326/Guess-My-Number',
-    // },
-    // {
-    //     id: 10,
-    //     project_image: 'https://raw.githubusercontent.com/PriyanshuSharma0326/Rock-Paper-Scissors/main/design/desktop-design.png',
-    //     project_title: 'Rock Paper Scissors',
-    //     live_url: 'https://priyanshusharma0326.github.io/Rock-Paper-Scissors/',
-    //     source_url: 'https://github.com/PriyanshuSharma0326/Rock-Paper-Scissors',
-    // },
 ];
 
-export {projects};
+export { projects };
